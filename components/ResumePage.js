9b4ed07caf1dd83906fcebe12d84a9136696aa63
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2022-02-15 00:42:02 -0800

    Adds all content and base styles to resume page

--- components/ResumePage.js
+++ components/ResumePage.js
@@ -6,9 +6,9 @@ import styles from "../styles/components/Resume.module.scss";
 
 function ResumePage() {
   return (
-    <section className={styles.resume}>
+    <article className={styles.resumePage}>
       <div className={styles.container}>
-        <div className={styles.about}>
+        <section className={styles.about}>
           <Link href={"#"} passHref>
             <a className={styles.print}>
               <FontAwesomeIcon icon={faPrint} />
@@ -33,34 +33,79 @@ function ResumePage() {
               <a target="_blank">{resumeContent.about.contact.phone}</a>
             </Link>
           </div>
+        </section>
+
+        <div className={styles.columns}>
+          <div className={styles.left}>
+            <section className={`${styles.education} ${styles.section}`}>
+              <h3 className={styles.sectionTitle}>Education</h3>
+              {resumeContent.education.map((cert, i) => {
+                return (
+                  <div className={styles.certification} key={i}>
+                    <h2>{cert.school}</h2>
+                    <h4>{cert.certification}</h4>
+                    <span>{cert.dateEnd}</span>
+                  </div>
+                );
+              })}
+            </section>
+            <section className={`${styles.keywords} ${styles.section}`}>
+              <h3 className={styles.sectionTitle}>Key Proficiencies</h3>
+              <ul>
+                {resumeContent.about.keywords.map((keyword, i) => {
+                  return (
+                    <li className={styles.keyword} key={i}>
+                      {keyword}
+                    </li>
+                  );
+                })}
+              </ul>
+            </section>
+          </div>
+          <div className={styles.right}>
+            <section className={`${styles.experience} ${styles.section}`}>
+              <h3 className={styles.sectionTitle}>Experience</h3>
+              {resumeContent.experience.map((position, i) => {
+                return (
+                  <div className={styles.position} key={i}>
+                    <div className={styles.meta}>
+                      <span>
+                        {position.dateStart}
+                        {" - "}
+                        {position.dateEnd ? position.dateEnd : "Present"}
+                      </span>
+                      <span>{position.type}</span>
+                      <span>{position.location}</span>
+                    </div>
+                    <h2 className={styles.title}>{position.title}</h2>
+                    <h4 className={styles.employer}>{position.employer}</h4>
+                  </div>
+                );
+              })}
+            </section>
+            <section className={`${styles.contracts} ${styles.section}`}>
+              <h3 className={styles.sectionTitle}>Contract Clients</h3>
+              {resumeContent.contracts.map((position, i) => {
+                return (
+                  <div className={styles.position} key={i}>
+                    <div className={styles.meta}>
+                      <span>
+                        {position.dateStart}
+                        {" - "}
+                        {position.dateEnd ? position.dateEnd : "Present"}
+                      </span>
+                      <span>{position.location}</span>
+                    </div>
+                    <h2 className={styles.employer}>{position.employer}</h2>
+                    <h4 className={styles.title}>{position.title}</h4>
+                  </div>
+                );
+              })}
+            </section>
+          </div>
         </div>
-        <div className={styles.keywords}>
-          <h3 className={styles.sectionTitle}>Key Proficiencies</h3>
-          <ul>
-            {resumeContent.about.keywords.map((keyword, i) => {
-              return (
-                <li className={styles.keyword} key={i}>
-                  {keyword}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        {/* Doesnt Make Sense to Use This Here */}
-        {/* <div className={styles.keywords}>
-          <h3 className={styles.sectionTitle}>Current Research</h3>
-          <ul>
-            {resumeContent.about.research.map((keyword, i) => {
-              return (
-                <li className={styles.keyword} key={i}>
-                  {keyword}
-                </li>
-              );
-            })}
-          </ul>
-        </div> */}
       </div>
-    </section>
+    </article>
   );
 }
 
